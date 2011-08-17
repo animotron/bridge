0ce5c540c0d80bfdeafa6e2471a40e3b4b93f611
Evgeny Gazdovsky <gazdovsky@gmail.com>
2011-08-17 16:43:34 +0400

    refactor web framework

--- src/main/java/org/animotron/bridge/AnimoServlet.java
+++ src/main/java/org/animotron/bridge/AnimoServlet.java
@@ -19,22 +19,23 @@
 package org.animotron.bridge;
 
 import org.animotron.AbstractExpression;
-import org.animotron.Executor;
 import org.animotron.Expression;
+import org.animotron.Statement;
 import org.animotron.exception.EBuilderTerminated;
 import org.animotron.exception.ENotFound;
 import org.animotron.graph.builder.CommonBuilder;
-import org.animotron.graph.handler.GraphHandler;
-import org.animotron.graph.handler.PipedGraphHandler;
+import org.animotron.graph.handler.BinaryGraphHandler;
+import org.animotron.graph.serializer.ResultSerializer;
 import org.animotron.graph.serializer.StringResultSerializer;
 import org.animotron.graph.traverser.GraphAnimoResultTraverser;
 import org.animotron.io.PipedInput;
-import org.animotron.io.PipedOutput;
+import org.animotron.manipulator.Evaluator;
 import org.animotron.operator.AN;
 import org.animotron.operator.THE;
 import org.animotron.operator.query.GET;
 import org.animotron.operator.relation.HAVE;
 import org.animotron.operator.relation.USE;
+import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 
 import javax.servlet.ServletException;
@@ -43,10 +44,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.sql.Statement;
 import java.util.Enumeration;
 
 import static org.animotron.Expression._;
+import static org.animotron.Properties.BIN;
 
 /**
  * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
@@ -56,7 +57,7 @@ public class AnimoServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 7276574723383015880L;
 
-	private void writeResponse(Relationship r, HttpServletResponse res) throws IOException {
+	private void writeResponse(Relationship r, HttpServletResponse res) throws IOException, ENotFound {
         WebSerializer.serialize(r, res);
     }
 
@@ -64,9 +65,14 @@ public class AnimoServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		long startTime = System.currentTimeMillis();
 		try {
-	        AnimoRequest a = new AnimoRequest(req);
-	        writeResponse(a, res);
-		} catch (EBuilderTerminated e) {
+	        writeResponse(new AnimoRequest(req), res);
+        } catch (ENotFound e) {
+            try {
+                writeResponse(new AnimoNotFound(req), res);
+            } catch (EBuilderTerminated eBuilderTerminated) {
+                throw new IOException(e);
+            }
+        } catch (EBuilderTerminated e) {
 			throw new IOException(e);
 		}
 		
@@ -78,153 +84,170 @@ public class AnimoServlet extends HttpServlet {
         Relationship r = null;
         try {
             r = CommonBuilder.build(req.getInputStream(), req.getRequestURI());
+            writeResponse(r, res);
         } catch (EBuilderTerminated e) {
             throw new ServletException(e);
         }
-        writeResponse(r, res);
 	}
 	
-	private class AnimoRequest extends AbstractExpression {
+    private class AnimoRequest extends AbstractExpression {
 
-		public AnimoRequest(HttpServletRequest req) throws EBuilderTerminated {
+        public AnimoRequest(HttpServletRequest req) throws EBuilderTerminated {
 
             super(false);
 
-            try {
+            startGraph();
 
-                startGraph();
+                start(AN._, "rest");
 
-                    start(AN._, "rest");
+                    String uri = req.getRequestURI();
+                    String[] parts = uri.split("/");
 
-                        String uri = req.getRequestURI();
-                        String[] parts = uri.split("/");
+                    boolean isRoot = true;
+                    for (String part : parts) {
+                        if (part.isEmpty()) continue;
+                        start(USE._, part);
+                        end();
+                        isRoot = false;
+                    }
 
-                        boolean isRoot = true;
-                        for (String part : parts) {
-                            if (part.isEmpty()) continue;
-                            start(USE._, part);
-                            end();
-                            isRoot = false;
-                        }
+                    if (isRoot) {
+                        start(USE._, "root");
+                        end();
+                    }
 
-                        if (isRoot) {
-                            start(USE._, "root");
-                            end();
-                        }
+                    Enumeration names = req.getParameterNames();
 
-                        Enumeration names = req.getParameterNames();
+                    while (names.hasMoreElements()) {
 
-                        while (names.hasMoreElements()) {
+                        String name = (String) names.nextElement();
+                        parts = name.split(":");
 
-                            String name = (String) names.nextElement();
-                            parts = name.split(":");
-
-                            if (parts.length > 1) {
-                                if (USE._.name().equals(parts[0])) {
-                                    start(USE._, parts[1]);
-                                } else {
-                                    start(HAVE._, parts[1]);
-                                }
+                        if (parts.length > 1) {
+                            if (USE._.name().equals(parts[0])) {
+                                start(USE._, parts[1]);
                             } else {
-                                start(HAVE._, name);
+                                start(HAVE._, parts[1]);
                             }
-                            for  (String value : req.getParameterValues(name)) {
-                                start(value);
-                                end();
-                            }
+                        } else {
+                            start(HAVE._, name);
+                        }
+                        for  (String value : req.getParameterValues(name)) {
+                            start(value);
                             end();
                         }
-
-                        start(HAVE._, "host");
-                            start(req.getServerName());
-                            end();
                         end();
-                        start(HAVE._, "uri");
-                            start(req.getRequestURI());
-                            end();
-                        end();
+                    }
 
-                    end();
-
-                endGraph();
-
-            } catch (ENotFound e) {
-
-                startGraph();
-                    start(AN._, "rest");
-                        start(USE._, "not-found");
-                        end();
-                        start(HAVE._, "host");
-                            start(req.getServerName());
-                            end();
-                        end();
-                        start(HAVE._, "uri");
-                            start(req.getRequestURI());
-                            end();
+                    start(HAVE._, "host");
+                        start(req.getServerName());
                         end();
                     end();
-                endGraph();
+                    start(HAVE._, "uri");
+                        start(req.getRequestURI());
+                        end();
+                    end();
 
-            }
+                end();
 
-		}
+            endGraph();
 
-	}
+        }
+
+    }
+
+    private static class AnimoNotFound extends AbstractExpression {
+
+        public AnimoNotFound(HttpServletRequest req) throws EBuilderTerminated {
+
+            startGraph();
+                start(AN._, "rest");
+                    start(USE._, "not-found");
+                    end();
+                    start(HAVE._, "host");
+                        start(req.getServerName());
+                        end();
+                    end();
+                    start(HAVE._, "uri");
+                        start(req.getRequestURI());
+                        end();
+                    end();
+                end();
+            endGraph();
+
+        }
+
+    }
 
     private static class WebSerializer {
 
-        public static void serialize(final Relationship r, HttpServletResponse res) throws IOException {
+        public static void serialize(final Relationship r, HttpServletResponse res) throws IOException, ENotFound {
 
-            res.setContentType("text/html");
-            OutputStream out = res.getOutputStream();
+            final OutputStream out = res.getOutputStream();
 
             try {
 
-                String mime = getMimeType(r);
+                Relationship mime = (Relationship) get(r, "mime-type");
+                Relationship content = (Relationship) get(r, "content");
+                String mimes = StringResultSerializer.serialize(r, mime);
 
-                final PipedInput pipe = new PipedInput();
+                if (content != null) {
 
-                Executor.execute(
-                    new Runnable() {
-                        @Override
-                        public void run() {
-                            GraphHandler handler = null;
-                            try {
-                                handler = new PipedGraphHandler(new PipedOutput(pipe));
-                                GraphAnimoResultTraverser._.traverse(handler, r);
-                            } catch (IOException e) {
-                                try {
-                                    pipe.close();
-                                } catch (IOException e1) {
-                                    e1.printStackTrace();
+                    res.setContentType(mimes == null ? "application/xml" :mimes);
+                    ResultSerializer.serialize(r, content, out);
+
+                } else {
+
+                    res.setContentType(mimes == null ? "application/octet-stream" :mimes);
+
+                    final boolean[] isNotFound = {true};
+
+                    GraphAnimoResultTraverser._.traverse(
+                        new BinaryGraphHandler(out){
+                            @Override
+                            public void start(Statement statement, Relationship r) throws IOException {
+                                Node n = r.getEndNode();
+                                if (BIN.has(n)) {
+                                    isNotFound[0] = false;
+                                    write(n, out);
                                 }
                             }
-                        }
-                    }
-                );
+                        }, r
+                    );
 
-                for (Object o : pipe) {
-                    Statement s = (Statement) o;
-                    Relationship op = (Relationship) pipe.read();
+                    if (isNotFound[0]) {
+
+                         throw new ENotFound("Resource not found");
+
+                    }
+
                 }
 
+            } catch (ENotFound e) {
+                throw e;
             } catch (EBuilderTerminated e) {
+                new IOException(e);
+            } catch (InterruptedException e) {
                 new IOException(e);
             }
 
 
         }
 
-        private static String getMimeType(Relationship r) throws IOException, EBuilderTerminated {
-            String m =  StringResultSerializer.serialize(
-                new Expression(
-                    _(GET._, "mime-type",
-                        _(AN._, THE._.name(r))
-                    )
+        private static Relationship get(Relationship r, String have) throws EBuilderTerminated, IOException, InterruptedException {
+            Expression get =  new Expression(
+                _(GET._, have,
+                    _(AN._, THE._.name(r))
                 )
             );
-            return "".equals(m) ? "aplication/animo+xml" : m;
-        }
+            PipedInput pipe = Evaluator._.execute(r, get);
+            for (Object o : pipe) {
+                pipe.close();
+                return (Relationship) o;
+            }
+            return null;
+        };
+
 
     }
 	

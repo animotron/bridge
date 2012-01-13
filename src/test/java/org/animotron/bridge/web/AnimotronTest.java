85ce1e9c15fddea60afab72b30fb2ebc365003b2
Dmitriy Shabanov <shabanovd@gmail.com>
2012-01-13 13:23:10 +0500

    fix typos

--- src/test/java/org/animotron/bridge/web/AnimotronTest.java
+++ src/test/java/org/animotron/bridge/web/AnimotronTest.java
@@ -58,7 +58,6 @@ public class AnimotronTest extends ATest {
 			)
         );
     	assertXMLResult(s, 
-			"<?xml version='1.0' encoding='UTF-8'?>" +
 			"<html>" +
 				"<head>" +
 					"<title>Welcome to Animo</title>" +

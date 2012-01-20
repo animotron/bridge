cc0e427e39c036afce2b41c9a67d935cf83dcf34
evgeny <evgeny@evgeny-laptop.(none)>
2012-01-20 10:09:43 +0400

    animo ide

--- apps/animoIDE/animo-ide.js
+++ apps/animoIDE/animo-ide.js
@@ -1,13 +1,10 @@
 (function($){
 
     $.fn.ide = function(){
-
         var self = $(this);
-
         var editor = ace.edit(self.get(0));
-
+        editor.
         return self;
-
     }
 
 })(jQuery);
